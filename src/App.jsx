6fceeddcc78bea33c7f0472f7d765f6237adb498
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-07-13 14:43:47 +0200

    fix loading in small screen

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,12 @@ function App() {
   };
 
   const renderContent = () => {
+    return (
+      <SplashScreen
+        value={spinnerValue}
+        totalTime={maximumValue * speedFactor}
+      />
+    );
     if (isLoading)
       return (
         <SplashScreen

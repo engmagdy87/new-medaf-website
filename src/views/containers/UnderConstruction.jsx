90c3fef884d9303f44c2e20b8c4bfe7d3b661b4d
Peter Atef <p.atef@hotmail.com>
2020-08-23 15:00:29 +0200

    changing under construction text

--- src/views/containers/UnderConstruction.jsx
+++ src/views/containers/UnderConstruction.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import UnderConstructionImage from '../../assets/images/under-construction.svg';
-import '../../assets/styles/containers/under-construction.scss';
+import React from "react";
+import UnderConstructionImage from "../../assets/images/under-construction.svg";
+import "../../assets/styles/containers/under-construction.scss";
 
 export default function UnderConstruction() {
   return (
     <div className="under-construction-wrapper">
       <img src={UnderConstructionImage} alt="Under Construction" />
       <h1>
-        <span>Under</span> Construction
+        Thank you for visiting MEDAF. <br />
+        Our mobile site is under development, until then, <br />
+        please open our website from your PC.
       </h1>
     </div>
   );
